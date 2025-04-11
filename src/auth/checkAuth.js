095c2c24dc27978dcf6703a38f59df12000aeff1
phamngocduy <ngocduy14062003@gmail.com>
2025-04-11 15:34:28 +0700

    add middlware apikey

---
+++ src/auth/checkAuth.js
@@ -0,0 +1,51 @@
+const Header = {
+  API_KEY: "x-api-key",
+  AUTHORIZATION: "authorization",
+};
+
+const { findById } = require("../services/apiKey.service");
+
+const apiKey = async (req, res, next) => {
+  try {
+    const apiKey = req.headers[Header.API_KEY];
+    if (!apiKey) {
+      return res.status(403).json({
+        message: "Forbidden error",
+      });
+    }
+    //check object key
+    const objKey = await findById(apiKey);
+    if (!objKey) {
+      return res.status(403).json({
+        message: "Forbidden error",
+      });
+    }
+    req.objKey = objKey;
+    next();
+  } catch (error) {}
+};
+
+// check permission
+const permission = (permission) => {
+  return (req, res, next) => {
+    const objKey = req.objKey;
+    if (!objKey.permissions.includes(permission)) {
+      return res.status(403).json({
+        message: "permission dinied",
+      });
+    }
+    console.log("Permission ::", permission);
+    const validPermission = req.objKey.permissions.includes(permission);
+    if (!validPermission) {
+      return res.status(403).json({
+        message: "permission dinied",
+      });
+    }
+    next();
+  };
+};
+
+module.exports = {
+  apiKey,
+  permission,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-
+const { apiKey, permission } = require("../auth/checkAuth");
+// check  apikey
+router.use(apiKey);
+//check permission
+router.use(permission("0000"));
 //sign up
 router.use("/v1/api", require("./access/index"));
 
