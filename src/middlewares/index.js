d1151b01b1e1315638e24131b3defcad65faf220
phamngocduy <ngocduy14062003@gmail.com>
2025-06-14 02:10:23 +0700

    fix message bot discord

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -5,7 +5,7 @@ const pushToLogDiscord = (req, res, next) => {
     Logger.sendToFormatCode({
       title: `Method: ${req.method}`,
       code: req.method === "GET" ? req.query : req.body,
-      message: `${req.get("host")}${req.originalUrl}`,
+      message: `${req.get("host")} ${req.originalUrl} - ${req.ip}`,
     });
     next();
   } catch (error) {
